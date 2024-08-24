6b26ba13917f596fad656ccda244741cac7815f7
Amanbanti <amanbanti2011@gmail.com>
2024-08-24 16:23:09 +0300

    register commit

--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,15 +1,18 @@
 import "./register.scss";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
 
 function Register() {
     const  [error,setError]  = useState("");
+    const  [IsLoading,setIsLoading]  = useState(false);
 
     const navigate = useNavigate()
 
     const handleSubmit = async (e) =>{
         e.preventDefault(); // Prevents the default form submission /prvent the data from posted on url
+        setError("")
+        setIsLoading(true);
         const formData = new FormData(e.target);
 
         const username = formData.get("username");
@@ -17,7 +20,7 @@ function Register() {
         const password = formData.get("password");
         
         try {
-            const res = await axios.post("http://localhost:8800/api/auth/register", {
+            const res = await apiRequest.post("/auth/register", {
               username, email, password
             });
 
@@ -26,6 +29,8 @@ function Register() {
           } catch (err) {
             console.error(err);
             setError(err.response.data.message)
+          }finally{
+            setIsLoading(false);
           }
           
     }
@@ -37,7 +42,7 @@ function Register() {
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button type="submit" >Register</button>
+          <button  disabled={IsLoading} type="submit" >Register</button>
           {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
@@ -45,7 +50,7 @@ function Register() {
       <div className="imgContainer">
         <img src="/bg.png" alt="" />
       </div>
-    </div>
+    </div> 
   );
 }
 
