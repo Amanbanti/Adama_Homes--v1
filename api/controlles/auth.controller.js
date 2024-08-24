605384c0288807cb2ec353fb9afa19f35b0d6436
Amanbanti <amanbanti2011@gmail.com>
2024-08-24 16:22:43 +0300

    apiRequest commit

--- api/controlles/auth.controller.js
+++ api/controlles/auth.controller.js
@@ -5,29 +5,32 @@ import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
-
+  
     try {
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create new user
-        const newUser = await prisma.user.create({
-            data: {
-                username,
-                email,
-                password: hashedPassword,
-            },
-        });
-
-        // console.log(newUser);
-
-        res.status(201).json({ message: "User created successfully" });
-
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      // Create new user
+      const newUser = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+  
+      res.status(201).json({ message: "User created successfully", user: newUser });
+  
     } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Failed to create user!" });
+      // Check for specific Prisma error codes
+      if (err.code === 'P2002' && err.meta && err.meta.target === 'user_email_key') {
+        return res.status(400).json({ message: "Email already in use!" });
+      }
+  
+      console.log(err);
+      res.status(500).json({ message: "Failed to create user!" });
     }
-};
+  };
 
 
 
@@ -55,6 +58,10 @@ export const login = async (req, res) => {
         const isPasswordValid = await bcrypt.compare(password,user.password);
 
         if(!isPasswordValid) return res.status(401).json({message: "Invalid Credentials!"});
+        
+        const {password: userpassword, ...userInfo} = user
+
+
 
         //generate cooke token and send to the user
         const maxAge = 60 * 60 * 24 * 7;  // 1 week in seconds
@@ -65,7 +72,7 @@ export const login = async (req, res) => {
         );
         
         // Correct syntax for setting the cookie
-        res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Max-Age=${maxAge}`).json("Success");
+        res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Max-Age=${maxAge}`).json(userInfo);
     }catch(err){
         console.log(err)
         res.status(500).json({message:"Failed to login!"})
